9b754a36c4d046069191e781ccd2c4fc5744737c
Unurjargal Boldbaatar <aja20.aja10success@gmail.com>
2026-02-15 00:49:53 -0500

    Update with nodemon, index.js and index.html

---
+++ public/js/index.js
@@ -0,0 +1,39 @@
+"use strict"
+
+const elements = {
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+};
+
+const quotes = [
+    {
+        quote: "Be yourself; everyone else is aready taken",
+        author: "Oscar Wilde",
+    },
+
+     {
+        quote: "Readers live a thousand lives before they die. A person who never reads lives only one.",
+        author: "George R.R. Martin",
+    },
+
+    {
+        quote: "If you only read the books that everyone else is reading, you can only think what everyone else is thinking.",
+        author: "Haruki Murakami",
+    }
+]
+
+
+function loopThroughQuotes() {
+    let quoteIndex = 0;
+    setInterval(() => {
+        if (quoteIndex < quotes.length) {
+            elements.quote.textContent = quotes[quoteIndex].quote;
+            elements.author.textContent = quotes[quoteIndex].author;
+            quoteIndex++;
+        } else {
+            quoteIndex = 0;
+        }
+    }, 3000);
+}
+   
+setTimeout(loopThroughQuotes, 3000);
